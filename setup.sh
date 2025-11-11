473707bbd0379bbbcff3f7d512968a5dec491dad
TacAttack-04 <goddlycat@gmail.com>
2025-11-10 19:18:37 -0600

    Refactor yay-bin installation and add PGP skip flag
    Updated installation process for yay-bin and added flag to skip PGP checks.

--- setup.sh
+++ setup.sh
@@ -13,7 +13,6 @@ pacman -S --noconfirm \
 sed -i '/^\#\[multilib\]/,/^\#Include/ s/^\#//' /etc/pacman.conf && \
 pacman -Sy --noconfirm && \
 
-# Build yay-bin (smaller bootstrap)
 su - $SUDO_USER << 'EOF'
 cd /tmp
 git clone https://aur.archlinux.org/yay-bin.git
@@ -21,9 +20,7 @@ cd yay-bin
 makepkg --noconfirm
 EOF
 
-# Install yay-bin and cleanup
 pacman -U --noconfirm /tmp/yay-bin/yay-bin-*.pkg.tar.zst && \
 rm -rf /tmp/yay-bin && \
 
-# Use yay to install everything else
-su - $SUDO_USER -c "yay -S --noconfirm paru-bin steam spotify curseforge-bin minecraft-launcher"
+su - $SUDO_USER -c "yay -S --noconfirm --mflags '--skippgpcheck' steam spotify curseforge-bin minecraft-launcher"
