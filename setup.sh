00bb9e218bd4272afd855e206937f384413f713c
TacAttack-04 <goddlycat@gmail.com>
2025-11-10 19:28:43 -0600

    Refactor setup.sh to install yay-bin and apps
    Updated setup script to install yay-bin and packages using sudo.

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ pacman -S --noconfirm \
 sed -i '/^\#\[multilib\]/,/^\#Include/ s/^\#//' /etc/pacman.conf && \
 pacman -Sy --noconfirm && \
 
+# Build yay-bin
 su - $SUDO_USER << 'EOF'
 cd /tmp
 git clone https://aur.archlinux.org/yay-bin.git
@@ -20,7 +21,13 @@ cd yay-bin
 makepkg --noconfirm
 EOF
 
+# Install yay-bin and cleanup
 pacman -U --noconfirm /tmp/yay-bin/yay-bin-*.pkg.tar.zst && \
 rm -rf /tmp/yay-bin && \
 
-su - $SUDO_USER -c "yay -S --noconfirm --mflags steam curseforge-bin minecraft-launcher"
+# Import Spotify key and install packages
+# Using sudo -u to preserve sudo credentials
+sudo -u $SUDO_USER bash << 'EOF'
+gpg --keyserver keyserver.ubuntu.com --recv-keys 5E3C45D7B312C643
+yay -S --noconfirm steam spotify curseforge-bin minecraft-launcher
+EOF
